d1111b7cf627151f3fff8ab4d8e6ef1ba9c608b0
pvirost <paulvirost@gmail.com>
2019-09-10 20:23:25 -0700

    10SEP2019

--- Project 3/js/app.js
+++ Project 3/js/app.js
@@ -117,12 +117,15 @@ window.player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+    if (Score < 3) {
+        var allowedKeys = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down'
+        }
+        player.handleInput(allowedKeys[e.keyCode]);
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    // player.handleInput(allowedKeys[e.keyCode]);
 });
