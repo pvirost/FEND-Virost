875f1987271e341b3e1db0779704e6aef853b810
pvirost <paulvirost@gmail.com>
2019-09-17 18:43:00 -0700

    Revert "MapBox API Add"
    This reverts commit f842063a3581af6c8ab77d4aee4564d6b2a9e61b.

--- Restaurant Reviews/js/main.js
+++ Restaurant Reviews/js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoicGF1bHZpcm9zdCIsImEiOiJjazBvN3FkMXAwN3JvM2Zwazd4NGR5ZWlhIn0.g7ZvkYCHjcrcr2wiuYCAFQ',
+    mapboxToken: '<your MAPBOX API KEY HERE>',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
