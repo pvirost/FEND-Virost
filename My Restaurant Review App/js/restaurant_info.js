eb011c2a55c6f63eb7fd935843bb388d0565a3a0
pvirost <paulvirost@gmail.com>
2019-09-17 19:39:46 -0700

    Working API and server on 8000

--- My Restaurant Review App/js/restaurant_info.js
+++ My Restaurant Review App/js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoicGF1bHZpcm9zdCIsImEiOiJjazBvbmZvcGwwY2t0M2NteDVkeDJpNzBzIn0.Z4z5Jezg9_jX7QBOv_3KVQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
